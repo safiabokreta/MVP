59ecf236d934adda1bee78749e1fcfee1848655b
safiabokreta <145928143+safiabokreta@users.noreply.github.com>
2024-12-07 00:42:26 +0100

    Add files via upload

---
+++ lib/views/themes/simpleStyle.dart/homeStyle.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/*
+import 'package:google_fonts/google_fonts.dart';
+import 'package:zefeffete/views/screens/Home/onboardingPages.dart';
+import 'package:zefeffete/views/widgets/bestVenues.dart';
+*/
+final welcomeMessage = GoogleFonts.raleway(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+final appName = GoogleFonts.raleway(
+  color: Color.fromARGB(255, 247, 117, 156),
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+final bestVendor =
+    GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold);
+final bestVenues =
+    GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold);
+
+final OnboardingPagesMessages =
+    GoogleFonts.raleway(fontSize: 18, fontWeight: FontWeight.w500);
+
+final seeMore = GoogleFonts.raleway(color: Colors.grey);
